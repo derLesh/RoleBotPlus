75234fe5257658713ab0df55eb9c639c4a856ca8
Lesh <LeshDev@users.noreply.github.com>
2018-01-28 18:10:14 +0100

    .

--- src/de/lesh/rolebotplus/utils/Token.java
+++ src/de/lesh/rolebotplus/utils/Token.java
@@ -1,7 +1,7 @@
 package de.lesh.rolebotplus.utils;
 
 public class Token {
-    private static final String token = "NDAxNzY5MDkzMDk2ODAwMjU4.DTvAKA.DtkvsHmOFJZ5VKImjKxUOZJRDf0";
+    private static final String token = "NDAxNzY5MDkzMDk2ODAwMjU4.DU-VUg.c2Zk-tSj-kJekFELUl9ExnAJZlU";
 
     public static String getToken() {
         return token;
