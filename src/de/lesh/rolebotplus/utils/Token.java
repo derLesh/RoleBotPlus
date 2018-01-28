6ad06e0d297163373ddf5f6d449ee6d81ec2613e
Lesh <LeshDev@users.noreply.github.com>
2018-01-28 18:09:00 +0100

    Delete Token.java

--- src/de/lesh/rolebotplus/utils/Token.java
+++
@@ -1,9 +0,0 @@
-package de.lesh.rolebotplus.utils;
-
-public class Token {
-    private static final String token = "NDAxNzY5MDkzMDk2ODAwMjU4.DTvAKA.DtkvsHmOFJZ5VKImjKxUOZJRDf0";
-
-    public static String getToken() {
-        return token;
-    }
-}
