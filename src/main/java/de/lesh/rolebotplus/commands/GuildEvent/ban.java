4015eca46a73a07c7f8194e71179130c479c732d
romangraef <roman.graef@gmail.com>
2018-01-30 15:27:59 +0100

    add ban reason

--- src/main/java/de/lesh/rolebotplus/commands/GuildEvent/ban.java
+++ src/main/java/de/lesh/rolebotplus/commands/GuildEvent/ban.java
@@ -2,9 +2,11 @@ package de.lesh.rolebotplus.commands.GuildEvent;
 
 import me.lesh.material.Red;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.audit.ActionType;
+import net.dv8tion.jda.core.audit.AuditLogEntry;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Guild.Ban;
-import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.guild.GuildBanEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import net.dv8tion.jda.core.requests.RestAction;
@@ -12,14 +14,21 @@ import net.dv8tion.jda.core.requests.RestAction;
 import java.util.List;
 
 public class ban extends ListenerAdapter {
-    Guild.Ban ban;
 
     public void onGuildBan(GuildBanEvent e) {
-
         EmbedBuilder eB = new EmbedBuilder();
-        RestAction<List<Ban>> banList = e.getGuild().getBanList();
         eB.addField("Gone forever - RIP " + e.getUser(), e.getUser().getName() + "#" + e.getUser().getDiscriminator() + "- Created: " + e.getUser().getCreationTime(), false);
-        eB.addField("Ban reason", "", false);
+        if (e.getGuild().getSelfMember().hasPermission(Permission.VIEW_AUDIT_LOGS)) {
+            List<AuditLogEntry> logEntries = e.getGuild().getAuditLogs()
+                    .type(ActionType.BAN)
+                    .user(e.getUser().getId())
+                    .limit(1)
+                    .complete();
+            if (logEntries.size() > 0) {
+                AuditLogEntry entry = logEntries.get(0);
+                eB.addField("Ban reason", entry.getReason(),false);
+            }
+        }
         eB.setColor(Red.r700);
         e.getGuild().getTextChannelById(316325706923507721L).sendMessage(eB.build()).queue();
     }
